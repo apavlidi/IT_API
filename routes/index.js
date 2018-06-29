29e26b46eb214692a49f700f2612f75bbb5ff99a
Vishnja Kostika <kvisnia@users.noreply.github.com>
2018-06-29 11:31:58 +0300

    Last minute fix 2

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router()
 const auth = require('../configs/auth')
 
 /* GET home page. */
-router.get('/', auth.checkAuth(true,["cn","id"],1,false), function (req, res, next) {
+router.get('/', auth.checkAuth(true,["cn","id"],1), function (req, res, next) {
   console.log(req.user)
   next({
     message: 'Unsupported get request.',
