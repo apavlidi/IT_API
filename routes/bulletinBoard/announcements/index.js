878295561439f462fa4fd294c4163342d3850c11
apavlidi <apavlidi@it.teithe.gr>
2018-07-05 20:37:42 +0300

    Merging

--- routes/bulletinBoard/announcements/index.js
+++ routes/bulletinBoard/announcements/index.js
@@ -26,7 +26,7 @@ function getAnnouncements (req, res, next) {
   apiFunctions.formatQuery(req.query).then(function (formatedQuery) {
     database.Announcements.find(formatedQuery.filters).select(formatedQuery.fields).populate('_about',
       'name public').populate('attachments', 'name').sort(formatedQuery.sort).exec(function (err, announcements) {
-      if (!err) {
+      if (err) {
         next(new ApplicationErrorClass('getAnnouncements', req.user.id, 100, err, 'Συνεβη καποιο λάθος κατα την λήψη ανακοινώσεων.', apiFunctions.getClientIp(req), 500))
       } else {
         res.status(200).json(announcements)
