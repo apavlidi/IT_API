ffea833c3939474df8cea555520b63cc347ffa15
apavlidi <apavlidi@it.teithe.gr>
2018-07-17 19:12:50 +0300

    changed location of notifications functions to notification hierarchy

--- routes/bulletinBoard/announcements/functions.js
+++ routes/bulletinBoard/announcements/functions.js
@@ -367,43 +367,6 @@ function findEmailsFromUserIds (registeredIds) {
     })
 }
 
-function sendNotifications (announcementEntry, notificationId, publisherId) {
-  return new Promise((resolve, reject) => {
-    let calls = []
-    database.AnnouncementsCategories.findOne({_id: announcementEntry._about}).exec(function (err, category) {
-      category.registered.forEach(function (id) {
-        calls.push(function (callback) {
-          database.Profile.findOne({
-            'ldapId': {$eq: id, $ne: publisherId}
-          }).exec(function (err, profile) {
-            if (!err && profile) {
-              //TODO THIS NEEDS TO BE CHECKED WHEN USER IS IMPLEMENTED
-              //sendPush.sendNotification(profile.notySub, announcementEntry, category)
-            }
-          })
-
-          database.Profile.update({'ldapId': {$eq: id, $ne: publisherId}}, {
-            '$addToSet': {
-              'notifications': {_notification: notificationId, seen: false}
-            }
-          }, function (err, updated) {
-            if (err) {
-              reject(new ApplicationErrorClass('insertNewAnnouncement', null, 109, err, 'Σφάλμα κατα την δημιουργία ανακοίνωσης.', null, 500))
-            }
-            callback(null)
-          })
-        })
-      })
-
-      async.parallel(calls, function (err) {
-        if (err) {
-          reject(new ApplicationErrorClass('insertNewAnnouncement', null, 110, err, 'Σφάλμα κατα την δημιουργία ανακοίνωσης.', null, 500))
-        }
-        resolve()
-      })
-    })
-  })
-}
 
 function generateWordpressContent (newAnnouncementId, text, textEn, attachments, date, publisher) {
   return new Promise((resolve, reject) => {
@@ -458,23 +421,6 @@ function formatDate (date, monthString) {
   return monthString ? (day + ' ' + monthNames[monthIndex] + ' ' + year + ' ' + hour + ':' + minute) : (day + '/' + monthIndex + '/' + year + ' ' + hour + ':' + minute)
 }
 
-function createNotification (announcementId, publisher) {
-  return new Promise((resolve, reject) => {
-    let notification = new database.Notification()
-    notification.userId = publisher.id
-    notification.nameEn = publisher.nameEn
-    notification.nameEl = publisher.nameEl
-    notification.related.id = announcementId
-    notification.save(function (err, newNotification) {
-      if (err) {
-        reject(new ApplicationErrorClass(null, null, 108, err, null, null, 500))
-      } else {
-        resolve(newNotification)
-      }
-    })
-  })
-}
-
 module.exports = {
   getAnnouncementsRSSPromise,
   validatePublisher,
@@ -482,7 +428,5 @@ module.exports = {
   checkIfEntryExists,
   gatherFilesInput,
   postToTeithe,
-  sendEmails,
-  sendNotifications,
-  createNotification
+  sendEmails
 }
