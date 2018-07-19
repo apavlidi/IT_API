f0adf55fd749410d26243f7015454affa0fe426d
apavlidi <apavlidi@it.teithe.gr>
2018-07-19 23:15:26 +0300

    Refactored /info Implemented /user/all

---
+++ configs/ldap.js
@@ -0,0 +1,5 @@
+const INTEGER_FIELDS = ['id', 'regyear', 'regsem', 'uidNumber', 'gidNumber', 'status']
+
+module.exports = {
+  INTEGER_FIELDS
+}
--- routes/user/user/function.js
+++ routes/user/user/function.js
@@ -1,10 +1,15 @@
 const crypt = require('crypt3/sync')
 const crypto = require('crypto')
+const async = require('async')
+const filter = require('ldap-filters')
+const text2png = require('text2png')
 
 const mailTexts = require('./../../../configs/mailText')
 const config = require('../../../configs/config')
 const database = require('../../../configs/database')
 const ApplicationErrorClass = require('../../applicationErrorClass')
+
+const INTEGER_FIELDS = require('./../../../configs/ldap').INTEGER_FIELDS
 
 function passwordsAreDifferent (password1, password2) {
   return password1 !== password2
@@ -114,6 +119,123 @@ function buildTokenAndMakeEntryForReset (user) {
     })
 }
 
+function appendDatabaseInfo (users) {
+  return new Promise(
+    function (resolve, reject) {
+      let calls = []
+      users.forEach(function (user) {
+        calls.push(function (callback) {
+          database.Profile.findOne({ldapId: user.id}, 'profilePhoto socialMedia', function (err, profile) {
+            if (err) {
+              reject(err)
+            } else {
+              if (profile) {
+                if (profile.profilePhoto && profile.profilePhoto.data) {
+                  user['profilePhoto'] = 'data:' + profile.profilePhoto.contentType + ';base64,' + new Buffer(profile.profilePhoto.data, 'base64').toString('binary')
+                } else {
+                  user['profilePhoto'] = ''
+                }
+                user['socialMedia'] = profile.socialMedia
+              }
+              callback(null)
+            }
+          })
+        })
+      })
+
+      async.parallel(calls, function (err) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(users)
+        }
+      })
+    })
+}
+
+function searchUsersOnLDAP (ldapMain, opts) {
+  return new Promise(
+    function (resolve, reject) {
+      ldapMain.search(config.LDAP[process.env.NODE_ENV].baseUserDN, opts, function (err, results) {
+        if (err) {
+          reject(err)
+        } else {
+          let usersArray = []
+          let userCounter = 0
+          results.on('searchEntry', function (user) {
+            userCounter++
+            addUserToArray(user, userCounter, usersArray)
+          })
+          results.on('error', function (err) {
+            (err.code === 4) ? resolve(usersArray) : reject()
+          })
+
+          results.on('end', function (result) {
+            resolve(usersArray)
+          })
+        }
+      })
+    })
+}
+
+function addUserToArray (user, userCounter, usersArray) {
+  let tmp = user.object
+  delete tmp.dn
+  delete tmp.controls
+  delete tmp.secondarymail
+  tmp.serNumber = userCounter
+
+  tmp.secondarymail = text2png(user.object.secondarymail, {
+    font: '14px Futura',
+    textColor: 'black',
+    bgColor: 'white',
+    lineSpacing: 1,
+    padding: 1,
+    output: 'dataURL'
+  })
+
+  usersArray.push(tmp)
+}
+
+function ldapSearchQueryFormat (query, id) {
+  return new Promise(
+    function (resolve, reject) {
+      delete query._
+      let output
+      let attr = ['id', 'displayName', 'description', 'secondarymail', 'eduPersonAffiliation', 'title', 'telephoneNumber', 'labeledURI', 'eduPersonEntitlement']
+      let searchAttr = [filter.attribute('eduPersonAffiliation').contains('staff')] //by default return only staff
+
+      if (Object.prototype.hasOwnProperty.call(query, 'fields')) {
+        delete query.fields
+      }
+
+      if (!(Object.keys(query).length === 0)) {
+        searchAttr = []
+        Object.keys(query).forEach(function (index) {
+          if (attr.indexOf(index) > -1) {
+            if (INTEGER_FIELDS.indexOf(index) > -1) {
+              searchAttr.push(filter.attribute(index).equalTo(query[index]))
+            } else if (index === 'labeledURI' || index === 'eduPersonEntitlement') {
+              searchAttr.push(filter.attribute(index).equalTo(query[index]))
+            }
+            else {
+              searchAttr.push(filter.attribute(index).contains(query[index]))
+            }
+          }
+        })
+      }
+
+      if (id) {
+        searchAttr.push(filter.attribute('id').equalTo(id))
+        output = filter.AND(searchAttr)
+      } else {
+        output = filter.OR(searchAttr)
+      }
+
+      resolve({filter: output.toString(), scope: 'sub', paged: {pageSize: 250, pagePause: false}, attributes: attr})
+    })
+}
+
 module.exports = {
   passwordsAreDifferent,
   oldPassIsCorrect,
@@ -123,5 +245,9 @@ module.exports = {
   validateIputForReset,
   buildTokenAndMakeEntryForReset,
   deleteResetToken,
-  passwordsAreSame
+  passwordsAreSame,
+  ldapSearchQueryFormat,
+  searchUsersOnLDAP,
+  appendDatabaseInfo
 }
+
