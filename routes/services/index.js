8ddf3c196a57b7c58c98b80265b346ca43d5f9b8
apavlidi <apavlidi@it.teithe.gr>
2018-08-14 15:40:02 +0300

    Finish services route

--- routes/services/index.js
+++ routes/services/index.js
@@ -11,6 +11,61 @@ const functions = require('./functions')
 let ldapMain = config.LDAP_CLIENT
 
 router.get('/', auth.checkAuth(['cn', 'id'], config.PERMISSIONS.student), getServiceStatus)
+router.patch('/ssh/users', auth.checkAuth(['cn', 'id'], config.PERMISSIONS.student), sshChangeStatusUsers)
+router.patch('/ssh/aetos', auth.checkAuth(['cn', 'id'], config.PERMISSIONS.student), sshChangeStatusAetos)
+
+function sshChangeStatusAetos (req, res, next) {
+  let ldapBinded = null
+  ldapFunctions.bindLdap(ldapMain).then(ldapMainBinded => {
+    ldapBinded = ldapMainBinded
+    let opts = ldapFunctions.buildOptions('(&(id=' + req.user.id + ')(objectClass=extensibleObject))', 'sub', ['info'])
+    return ldapFunctions.searchUserOnLDAP(ldapBinded, opts)
+  }).then(user => {
+    if (functions.usersExistsAndIsActive(user)) {
+      if (functions.sshIsActivatedOnVM(user, 'aetos')) {
+        return functions.disableSshFromVM(ldapBinded, user, 'aetos')
+      } else {
+        return functions.enableSshFromVm(ldapBinded, user, 'aetos')
+      }
+    } else {
+      throw new ApplicationErrorClass('sshChangeStatusAetos', null, 0, null, 'Ο χρήστης δεν βρέθηκε ή δεν είναι ενεργοποιημένος.', apiFunctions.getClientIp(req), 500)
+    }
+  }).then(() => {
+    res.sendStatus(200)
+  }).catch(applicationError => {
+    applicationError.type = 'sshChangeStatusAetos'
+    applicationError.user = req.user.id
+    applicationError.ip = apiFunctions.getClientIp(req)
+    next(applicationError)
+  })
+}
+
+function sshChangeStatusUsers (req, res, next) {
+  let ldapBinded = null
+  ldapFunctions.bindLdap(ldapMain).then(ldapMainBinded => {
+    ldapBinded = ldapMainBinded
+    let opts = ldapFunctions.buildOptions('(&(id=' + req.user.id + ')(objectClass=extensibleObject))', 'sub', ['info'])
+    return ldapFunctions.searchUserOnLDAP(ldapBinded, opts)
+  }).then(user => {
+    if (functions.usersExistsAndIsActive(user)) {
+      if (functions.sshIsActivatedOnVM(user, 'users')) {
+        return functions.disableSshFromVM(ldapBinded, user, 'users')
+      } else {
+        return functions.enableSshFromVm(ldapBinded, user, 'users')
+
+      }
+    } else {
+      throw new ApplicationErrorClass('sshChangeStatusUsers', null, 0, null, 'Ο χρήστης δεν βρέθηκε ή δεν είναι ενεργοποιημένος.', apiFunctions.getClientIp(req), 500)
+    }
+  }).then(() => {
+    res.sendStatus(200)
+  }).catch(applicationError => {
+    applicationError.type = 'sshChangeStatusUsers'
+    applicationError.user = req.user.id
+    applicationError.ip = apiFunctions.getClientIp(req)
+    next(applicationError)
+  })
+}
 
 function getServiceStatus (req, res, next) {
   let ldapBinded = null
@@ -40,6 +95,11 @@ function getServiceStatus (req, res, next) {
     } else {
       next(new ApplicationErrorClass('getServiceStatus', req.user.id, 0, null, 'Ο χρήστης δεν βρέθηκε', apiFunctions.getClientIp(req), 500))
     }
+  }).catch(applicationError => {
+    applicationError.type = 'getServiceStatus'
+    applicationError.user = req.user.id
+    applicationError.ip = apiFunctions.getClientIp(req)
+    next(applicationError)
   })
 }
 
