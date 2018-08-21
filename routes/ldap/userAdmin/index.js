ae16011fd147b23e9a191c23b51f3cbb54898d11
apavlidi <apavlidi@it.teithe.gr>
2018-08-21 17:29:00 +0300

    Change permissions to route based

--- routes/ldap/userAdmin/index.js
+++ routes/ldap/userAdmin/index.js
@@ -13,10 +13,10 @@ const ldapConfig = require('../../../configs/ldap')
 
 let ldapMain = config.LDAP_CLIENT
 
-router.get('/:id', auth.checkAuth(['cn', 'id'], config.PERMISSIONS.professorWithMaxAccess), getUser)
-router.post('/', auth.checkAuth(['cn', 'id'], config.PERMISSIONS.professorWithMaxAccess), apiFunctions.validateInput('body', validSchemas.addUser), addUser)
-router.delete('/:id', auth.checkAuth(['cn', 'id'], config.PERMISSIONS.professorWithMaxAccess), deleteUser)
-router.patch('/:id', auth.checkAuth(['cn', 'id'], config.PERMISSIONS.professorWithMaxAccess), apiFunctions.validateInput('body', validSchemas.updateUser), updateUser)
+router.get('/:id', auth.checkAuth(['ldap'], config.PERMISSIONS.professorWithMaxAccess), getUser)
+router.post('/', auth.checkAuth(['ldap'], config.PERMISSIONS.professorWithMaxAccess), apiFunctions.validateInput('body', validSchemas.addUser), addUser)
+router.delete('/:id', auth.checkAuth(['ldap'], config.PERMISSIONS.professorWithMaxAccess), deleteUser)
+router.patch('/:id', auth.checkAuth(['ldap'], config.PERMISSIONS.professorWithMaxAccess), apiFunctions.validateInput('body', validSchemas.updateUser), updateUser)
 
 function updateUser (req, res, next) {
   let attr = req.body.attr
--- routes/user/profileNoty/index.js
+++ routes/user/profileNoty/index.js
@@ -9,9 +9,9 @@ const config = require('../../../configs/config')
 const ApplicationErrorClass = require('./../../applicationErrorClass')
 const validSchemas = require('./joi')
 
-router.get('/', auth.checkAuth(['cn', 'id'], config.PERMISSIONS.student), getNotySub)
-router.patch('/', auth.checkAuth(['cn', 'id'], config.PERMISSIONS.student), apiFunctions.validateInput('body', validSchemas.enableNotySub), enableNotySub)
-router.delete('/', auth.checkAuth(['cn', 'id'], config.PERMISSIONS.student), apiFunctions.validateInput('body', validSchemas.disableNotySub), disableNotySub)
+router.get('/', auth.checkAuth(['user'], config.PERMISSIONS.student), getNotySub)
+router.patch('/', auth.checkAuth(['user'], config.PERMISSIONS.student), apiFunctions.validateInput('body', validSchemas.enableNotySub), enableNotySub)
+router.delete('/', auth.checkAuth(['user'], config.PERMISSIONS.student), apiFunctions.validateInput('body', validSchemas.disableNotySub), disableNotySub)
 
 // TODO REFACTOR
 function disableNotySub (req, res, next) {
